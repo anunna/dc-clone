077f8f56e47ec9bd1c2cecf97b99de4886476460
anunna <anunna@lilitusec.net>
2023-11-30 22:58:35 +0300

    additional setup work

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -2,11 +2,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div>
       <p className="text-3xl font-bold text-indigo-500">
         Hello Discord Clone
       </p>
-      <Button>Click me</Button>
+      <Button>
+      Click me
+      </Button>
     </div>
   )
 }
